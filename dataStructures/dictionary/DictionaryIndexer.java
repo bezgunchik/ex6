6999727290833ec0a9c3003c23463285a85ababd
unknown <ilia.bezgin@mauk.huji.ac.il>
2019-06-13 23:15:10 +0300

    'evening13.06'

--- dataStructures/dictionary/DictionaryIndexer.java
+++ dataStructures/dictionary/DictionaryIndexer.java
@@ -3,13 +3,18 @@ package dataStructures.dictionary;
 import dataStructures.Aindexer;
 import processing.parsingRules.IparsingRule;
 import processing.searchStrategies.DictionarySearch;
+import processing.textStructure.Block;
 import processing.textStructure.Corpus;
 import processing.textStructure.Entry;
 import processing.textStructure.Word;
+import utils.Stemmer;
 import utils.WrongMD5ChecksumException;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * An implementation of the abstract Aindexer class, backed by a simple hashmap to store words and their
@@ -17,7 +22,7 @@ import java.util.HashMap;
  */
 public class DictionaryIndexer extends Aindexer<DictionarySearch> {
 
-	private HashMap<String, Word> dict;
+	private HashMap<String, List<Word>> dict;
 
 	/**
 	 * Basic constructor, sets origin Corpus and initializes backing hashmap
@@ -35,8 +40,28 @@ public class DictionaryIndexer extends Aindexer<DictionarySearch> {
 
 	@Override
 	protected void indexCorpus() {
+		Stemmer stemmer = new Stemmer();
 		for (Entry entry : this.getCorpus()) {
-			getParseRule().parseFile(entry)
+			for (Block block : entry) {
+				try {
+					block.getRAF().seek(block.getStartIndex());
+					String line;
+					String[] words;
+					while ((line = block.getRAF().readLine()) != null)  {
+						words = line.split("\\W");
+						for (String word : words) {
+							String base = stemmer.stem(word);
+							long strIndex = block.getRAF().getFilePointer();
+							// TODO now the end index is the start index + length of the original word (may be it should be length of the base)
+							dict.get(base).add(new Word(block, strIndex, strIndex + word.length()));
+						}
+					}
+
+				} catch (IOException IOEx) {
+					// TODO to do something
+				}
+			}
+//			getParseRule().parseFile(entry)
 		}
 
 	}
